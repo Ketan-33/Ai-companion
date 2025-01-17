0c4db6e4df162640bb711d987b89350a9322048f
Ketan Pardhi <134635206+Ketan-33@users.noreply.github.com>
2025-01-17 18:53:24 +0000

    Integrate Clerk authentication :)

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 const Page = () => {
   return (
     <div>
-      <h1>Welcome to My Page</h1>
+      <UserButton afterSignOutUrl="/sign-in"/>
+      <h1>Home Page (Procected)</h1>
     </div>
   );
 };
