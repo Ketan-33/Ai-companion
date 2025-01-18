f4eef825a5ec48ec738f027346a0272d2361914d
Ketan Pardhi <134635206+Ketan-33@users.noreply.github.com>
2025-01-18 20:32:16 +0000

    Add search input component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
+import { cn } from '@/lib/utils'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body>
+        <body className={cn("bg-secondary")}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
